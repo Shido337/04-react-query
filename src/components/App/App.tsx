83de51224320aad9a716ec3510a72c1c1c55aa2a
Русальчук Андрей <anrusalchuk@gmail.com>
2026-03-16 23:38:13 +0200

    04-react-query

---
+++ src/components/App/App.tsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+import _ReactPaginate from "react-paginate";
+const ReactPaginate = (_ReactPaginate as any).default ?? _ReactPaginate;
+import toast, { Toaster } from "react-hot-toast";
+import css from "./App.module.css";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["movies", query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: !!query,
+  });
+
+  useEffect(() => {
+    if (!isLoading && !isError && data?.results.length === 0 && query) {
+      toast.error("No movies found for your request.");
+    }
+  }, [data, isLoading, isError, query]);
+
+  const handleSearch = (newQuery: string) => {
+    setQuery(newQuery);
+    setPage(1);
+  };
+
+  return (
+    <div className={css.app}>
+      <SearchBar onSearch={handleSearch} />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+
+      {!isLoading &&
+        !isError &&
+        data?.results.length === 0 &&
+        query &&
+        toast.error("No movies found for your request.")}
+
+      {!isLoading && !isError && data?.results.length > 0 && (
+        <>
+          <MovieGrid movies={data.results} onSelect={setSelectedMovie} />
+
+          {data.total_pages > 1 && (
+            <ReactPaginate
+              pageCount={data.total_pages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={css.pagination}
+              activeClassName={css.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+        </>
+      )}
+
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+
+      <Toaster position="top-right" />
+    </div>
+  );
+};
+
+export default App;
